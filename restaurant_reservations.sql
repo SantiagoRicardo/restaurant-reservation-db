ce5940688f1ff3742d7810eb72a1b38fc5a3e3ca
Santiago <94275573+SantiagoRicardo@users.noreply.github.com>
2024-06-07 17:50:18 -0500

    Update restaurant_reservations.sql

--- restaurant_reservations.sql
+++ restaurant_reservations.sql
@@ -12,7 +12,14 @@ CREATE TABLE reservations (
 );
 
 INSERT INTO reservations (customer_name, number_of_people, reservation_datetime, status)
-VALUES ('John Doe', 4, '2024-06-10 19:00:00', 'active');
-
-INSERT INTO reservations (customer_name, number_of_people, reservation_datetime, status)
-VALUES ('Jane Smith', 2, '2024-06-10 20:00:00', 'active');
+VALUES 
+('Jane Smith', 2, '2024-06-10 20:00:00', 'Active'),
+('Roberto Sánchez', 7, '2024-06-12 05:30:00', 'Confirmed'),
+('Ana Garcia', 8, '2024-06-13 14:00:00', 'Confirmed'),
+('David Williams', 1, '2024-06-20 13:15:00', 'Cancelled'),
+('Emily Johnson', 4, '2024-07-21 15:00:00', 'Pending'),
+('Jessica Miller', 3, '2024-06-11 09:30:00', 'Active'),
+('Michael Brown', 5, '2024-07-15 10:30:00', 'Pending'),
+('Laura Martinez', 6, '2024-07-18 16:45:00', 'Cancelled'),
+('Olivia Thomas', 2, '2024-07-19 19:20:00', 'Confirmed'),
+('Ethan Gonzalez', 3, '2024-07-22 08:15:00', 'Active');
